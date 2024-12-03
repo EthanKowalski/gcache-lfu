4bbf5ddba95463ccba59351783a61e9bf7bf9c96
EthanKowalski <ethankowalski123@gmail.com>
2024-12-03 13:39:10 -0600

    added lfu unit test

--- tests/test_lfu.cpp
+++ tests/test_lfu.cpp
@@ -23,38 +23,38 @@ void test() {
   assert(cache.size() == 0);
   assert(cache.capacity() == 4);
 
-  auto h1 = cache.insert(1, true);
+  auto h1 = cache.insert(1, true); // f1 = 1
   assert(h1);
   assert(cache.size() == 1);
   *h1 = 111;
-  auto h2 = cache.insert(2, true);
+  auto h2 = cache.insert(2, true); // f2 = 1
   assert(h2);
   assert(cache.size() == 2);
   *h1 = 222;
-  auto h3 = cache.insert(3, true);
+  auto h3 = cache.insert(3, true); // f3 = 1
   assert(h3);
   assert(cache.size() == 3);
   *h1 = 333;
-  auto h4 = cache.insert(4);
+  auto h4 = cache.insert(4); // f4 = 1
   assert(h4);
   assert(cache.size() == 4);
   *h1 = 444;
-  std::cout << "=== Expect: lru: [4], in_use: [1, 2, 3] ===\n";
+  std::cout << "=== Expect: lfu: [4], in_use: [1, 2, 3] ===\n";
   std::cout << cache;
 
-  h4 = cache.lookup(4, true);
+  h4 = cache.lookup(4, true); // f4 = 2
   *h4 = 4444;
   assert(cache.size() == 4);
-  std::cout << "=== Expect: lru: [], in_use: [1, 2, 3, 4] ===\n";
+  std::cout << "=== Expect: lfu: [], in_use: [1, 2, 3, 4] ===\n";
   std::cout << cache;
 
-  auto h5 = cache.insert(5, true);
+  auto h5 = cache.insert(5, true); // does not insert
   if (h5 != nullptr)
     throw std::runtime_error("Overflow insertion is not denied!");
   assert(cache.size() == 4);
 
   cache.release(h3);
-  h5 = cache.insert(5, true);
+  h5 = cache.insert(5, true); // f5 = 1
   assert(h5);
   assert(cache.size() == 4);
   *h5 = 555;
@@ -65,66 +65,86 @@ void test() {
   cache.release(h2);
   cache.release(h4);
   assert(cache.size() == 4);
-  std::cout << "\n=== Expect: lru: [5, 2, 4], in_use: [1] ===\n";
+  std::cout << "\n=== Expect: lfu: [5, 2, 4], in_use: [1] ===\n";
   std::cout << cache;
 
-  h3 = cache.insert(3, true);
+  h3 = cache.insert(3, true); // f3 = 1
   assert(h3);
   assert(cache.size() == 4);
   *h3 = 3333;
-  h5 = cache.lookup(5, true);
+  h5 = cache.lookup(5, true); // does not exist
   assert(cache.size() == 4);
-  std::cout << "\n=== Expect: lru: [2, 4], in_use: [1, 3] ===\n";
+  std::cout << "\n=== Expect: lfu: [2, 4], in_use: [1, 3] ===\n";
   std::cout << cache;
   if (h5 != nullptr)
     throw std::runtime_error("Expected evicted handle remains in cache!");
 
-  h5 = cache.insert(5, true);
+  h5 = cache.insert(5, true); // f5 = 1
   assert(cache.size() == 4);
-  std::cout << "\n=== Expect: lru: [4], in_use: [1, 3, 5] ===\n";
+  std::cout << "\n=== Expect: lfu: [4], in_use: [1, 3, 5] ===\n";
   std::cout << cache;
 
-  auto h6 = cache.insert(6, true);
+  auto h6 = cache.insert(6, true); // f6 = 1
   assert(h6);
   assert(cache.size() == 4);
   *h6 = 666;
-  std::cout << "\n=== Expect: lru: [], in_use: [1, 3, 5, 6] ===\n";
+  std::cout << "\n=== Expect: lfu: [], in_use: [1, 3, 5, 6] ===\n";
   std::cout << cache;
 
-  auto h5_ = cache.insert(5, true);
+  auto h5_ = cache.insert(5, true); // f5 = 2 (using different handle)
   assert(h5_ == h5);
   assert(cache.size() == 4);
   *h5_ = 555;
-  std::cout << "\n=== Expect: lru: [], in_use: [1, 3, 5, 6] ===\n";
+  std::cout << "\n=== Expect: lfu: [], in_use: [1, 3, 5, 6] ===\n";
   std::cout << cache;
 
-  auto h7 = cache.insert(7, true);
+  auto h7 = cache.insert(7, true); // does not insert
   assert(cache.size() == 4);
-  std::cout << "\n=== Expect: lru: [], in_use: [1, 3, 5, 6] ===\n";
+  std::cout << "\n=== Expect: lfu: [], in_use: [1, 3, 5, 6] ===\n";
   std::cout << cache;
   if (h7) throw std::runtime_error("Overflow handle is not denied!");
 
   cache.release(h1);
   cache.release(h3);
-  cache.release(h5);
+  cache.release(h5); // handle h5_ is still pinned
   cache.release(h6);
   assert(cache.size() == 4);
-  std::cout << "\n=== Expect: lru: [1, 3, 6], in_use: [5] ===\n";
+  std::cout << "\n=== Expect: lfu: [1, 3, 6], in_use: [5] ===\n";
+  std::cout << cache;
+
+  h3 = cache.lookup(3); // f3 = 2
+  assert(cache.size() == 4);
+  std::cout << "\n=== Expect: lfu: [1, 6, 3], in_use: [5] ===\n";
   std::cout << cache;
 
   cache.release(h5_);
   assert(cache.size() == 4);
-  std::cout << "\n=== Expect: lru: [1, 3, 6, 5], in_use: [] ===\n";
+  std::cout << "\n=== Expect: lfu: [1, 6, 3, 5], in_use: [] ===\n";
+  std::cout << cache;
+
+  h5 = cache.lookup(5); // f5 = 3
+  assert(cache.size() == 4);
+  std::cout << "\n=== Expect: lfu: [1, 6, 3, 5], in_use: [] ===\n";
+  std::cout << cache;
+
+  h6 = cache.lookup(6, true); // f6 = 2
+  assert(cache.size() == 4);
+  std::cout << "\n=== Expect: lfu: [1, 3, 5], in_use: [6] ===\n";
+  std::cout << cache;
+
+  cache.release(h6);
+  assert(cache.size() == 4);
+  std::cout << "\n=== Expect: lfu: [1, 3, 6, 5], in_use: [] ===\n";
   std::cout << cache;
 
   h7 = cache.lookup(7);
   if (h7) throw std::runtime_error("Lookup nonexisting handle is not denied!");
 
-  h7 = cache.insert(7);
+  h7 = cache.insert(7); // f7 = 1
   assert(h7);
   assert(cache.size() == 4);
   *h7 = 777;
-  std::cout << "\n=== Expect: lru: [3, 6, 5, 7], in_use: [] ===\n";
+  std::cout << "\n=== Expect: lfu: [3, 6, 5, 7], in_use: [] ===\n";
   std::cout << cache;
 
   // test erase/install
@@ -132,29 +152,29 @@ void test() {
   assert(success);
   assert(cache.size() == 3);
   assert(cache.capacity() == 3);
-  std::cout << "\n=== Expect: lru: [3, 6, 5], in_use: [] ===\n";
+  std::cout << "\n=== Expect: lfu: [3, 6, 5], in_use: [] ===\n";
   std::cout << cache;
 
-  h6 = cache.lookup(6, true);
+  h6 = cache.lookup(6, true); // f6 = 3
   assert(h6);
   assert(cache.size() == 3);
-  std::cout << "\n=== Expect: lru: [3, 5], in_use: [6] ===\n";
+  std::cout << "\n=== Expect: lfu: [3, 5], in_use: [6] ===\n";
   std::cout << cache;
   success = cache.erase(h6);
   if (success) throw std::runtime_error("Erase in-use handle is not denied!");
 
-  auto h8 = cache.insert(8);
+  auto h8 = cache.insert(8); // f8 = 1
   *h8 = 888;
   assert(cache.size() == 3);
-  std::cout << "\n=== Expect: lru: [5, 8], in_use: [6] ===\n";
+  std::cout << "\n=== Expect: lfu: [5, 8], in_use: [6] ===\n";
   std::cout << cache;
 
-  auto h9 = cache.install(9);
+  auto h9 = cache.install(9); // f9 = 1
   assert(h9);
   assert(cache.size() == 4);
   assert(cache.capacity() == 4);
   *h9 = 999;
-  std::cout << "\n=== Expect: lru: [5, 8, 9], in_use: [6] ===\n";
+  std::cout << "\n=== Expect: lfu: [5, 8, 9], in_use: [6] ===\n";
   std::cout << cache;
 
   // test for_each
@@ -166,8 +186,10 @@ void test() {
   std::cout << "}" << std::endl;
 
   cache.release(h6);
+  std::cout << "\n=== Expect: lfu: [5, 6, 8, 9], in_use: [] ===\n";
+  std::cout << cache;
 
-  // test for checkpoint and recover
+  // test for checkpoint and recover (would keep order, but lose frequency?)
   std::vector<std::pair<uint32_t, uint32_t>> ckpt;
   cache.for_each_lfu([&ckpt](LFUCache<uint32_t, uint32_t, hash1>::Handle_t h) {
     ckpt.emplace_back(h.get_key(), *h);
@@ -176,12 +198,12 @@ void test() {
   LFUCache<uint32_t, uint32_t, hash1> cache_recovered;
   cache_recovered.init(4);
   for (auto& [key, value] : ckpt) {
-    auto h = cache_recovered.insert(key, true);
+    auto h = cache_recovered.insert(key, true); // all f = 1 on recovery
     assert(h);
     *h = value;
     cache_recovered.release(h);
   }
-  std::cout << "\n=== Expect: { 5: 555, 8: 888, 9: 999, 6: 666, } ===\n";
+  std::cout << "\n=== Expect: { 5: 555, 6: 666, 8: 888, 9: 999, } ===\n";
   std::cout << cache_recovered;
 
   std::cout << std::flush;
